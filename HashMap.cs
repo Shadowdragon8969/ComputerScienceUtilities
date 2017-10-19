d0f64edccc7cd185de608524238806ebeeb5fcd3
Shadowdragon8969 <shadowdragon8969@gmail.com>
2017-10-19 14:25:54 -0500

    HashMaps are in!!!
    HashMaps are a variable type native to java. It allowed you to store variables in sets and gave you the ability to get a value when you only knew one half of the operation. A demonstration of how to use is below.
    
    public HashMap map = new HashMap();
    
    //The line below would store the text of a label by assigning it to a string called "LabelText"
    public void storeLabelText(Label label) {
              map.put("LabelText", label.Text);
    }
    
    // Once that the data is stored, you are free to call upon it whenever
    private void button1_Click(object sender, EventArgs e) {
               label1.Text = map.get("LabelText").ToString();
    }
    
    Remember to use the Set.cs class I provided as well or the HashMaps will not work!

---
+++ HashMap.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerScience
+{
+    public class HashMap
+    {
+        private static int ids = 0;
+        private int id;
+        private List<Set> keySet = new List<Set>();
+
+        public HashMap() {
+            id = ids;
+            ids++;
+        }
+
+        public void put(Object key, Object value) {
+            foreach (Set s in keySet) {
+                if (s.getKey() == key) {
+                    return;
+                }
+            }
+            Set set = new Set(key, value);
+            keySet.Add(set);
+        }
+
+        public Boolean containsKey(Object key) {
+            foreach (Set s in keySet) {
+                if (s.getKey() == key)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<Object> getKeys() {
+            List<Object> keys = new List<Object>();
+            foreach (Set s in keySet) {
+                keys.Add(s.getKey());
+            }
+            return keys;
+        }
+
+        public List<Object> getValues() {
+            List<Object> keys = new List<Object>();
+            foreach (Set s in keySet)
+            {
+                keys.Add(s.getValue());
+            }
+            return keys;
+        }
+
+        public Object get(object key) {
+            foreach (Set s in keySet)
+            {
+                if (s.getKey() == key)
+                {
+                    return s.getValue();
+                }
+            }
+            return null;
+        }
+
+    }
+}
