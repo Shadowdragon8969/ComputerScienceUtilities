c84900ffd2ba1270797ebd01fd35b5f57495b834
Shadowdragon8969 <shadowdragon8969@gmail.com>
2017-10-24 15:24:02 -0500

    Create Entity.cs

---
+++ Entity.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerScienceUtilities
+{
+    public class Entity
+    {
+
+        private static HashMap playerWorld = new HashMap();
+
+        private String uuid;
+        private bool moveable = true;
+        private PictureBox box;
+        private EntityLocation el;
+        private bool dead = false;
+        private bool spawned = false;
+        private bool textured = false;
+        private String texturePath;
+
+        public Entity(PictureBox box) {
+            this.box = box;
+            
+        }
+
+        public void setTexture(String path) {
+            Bitmap bm = new Bitmap(path);
+            box.Image = bm;
+            this.texturePath = path;
+            box.Size = bm.Size;
+            textured = true;
+            box.BackColor = Color.Transparent;
+        }
+
+        public Boolean hasTexture()
+        {
+            return textured;
+        }
+
+        public void setMovable() {
+            moveable = true;
+        }
+
+        public void spawnAtLocation(Location l) {
+            box.Location = new Point(l.getX(), l.getY());
+            playerWorld.put(this, l.getWorld());
+            el = new EntityLocation(l.getWorld(), l.getX(), l.getY(), this);
+            spawned = true;
+        }
+
+        public EntityLocation getLocation() {
+            if (spawned == true)
+            {
+                return el;
+            }
+            else {
+                return null;
+            }
+        }
+
+        public void applyVectors() {
+            if (el.getVectorY().getForce() == 0) el.getVectorY().setSpeed(0);
+            if (el.getVectorX().getForce() == 0) el.getVectorX().setSpeed(0);
+            if (el.getVectorY().getForce() > 0) el.getVectorY().setForce(el.getVectorY().getForce() - 1);
+            if (el.getVectorX().getForce() > 0) el.getVectorX().setForce(el.getVectorX().getForce() - 1);
+            box.Location = new Point(box.Location.X + el.getVectorX().getSpeed(), box.Location.Y + el.getVectorY().getSpeed());
+        }
+
+    }
+}
