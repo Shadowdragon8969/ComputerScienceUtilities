562ddb1a65e6d66d16c07f364aa1eed6bc7131c4
Shadowdragon8969 <shadowdragon8969@gmail.com>
2017-10-26 15:11:18 -0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -12,44 +12,66 @@ namespace ComputerScienceUtilities
 {
     public partial class Form1 : Form
     {
+        // The timer that we will use to create repeating events
         Timer t = new Timer();
-        public static List<Keys> keysDown = new List<Keys>();
+        // Your computers instance of the game
+        Client client = Client.getClient();
+        // The entity that will be used as the player in this example
         Entity e;
+        // The world that the entity will exist in
         World w;
 
         public Form1()
         {
             InitializeComponent();
+            // This section makes sure that the timer fires every 20 milliseconds
             t.Tick += new EventHandler(worldTickElapsed);
             t.Interval = 20;
             t.Enabled = true;
             t.Start();
+            ///////////////////////////////////////////////////////////////////////
+            /* This sets the world properties
+             * 'pictureBox2' is the PictureBox that will be used as the background
+             * 'this' is the Form that the world will use
+             * '100' is the Y level that is the ground, in this example, the ground is
+             * 100 pixels down
+             * '3' is gravity level that will push any entities that are effected by gravity
+             * to the ground, the higher the number, the faster they fall!
+            */
             w = new World(pictureBox2, this, "", 100, 3);
+            // This sets the PictureBox that will be used as the player
             e = new Entity(pictureBox1);
+            // This spawns the player at a certain location on the world
             e.spawnAtLocation(new Location(w, 20, 50));
         }
 
+        // The repeating loop that fires every 20 milliseconds
         private void worldTickElapsed(object sender, EventArgs e)
         {
             t.Stop();
             /* This is the game timer, events are repeated between the start and stop lines */
+
+            //Makes the entity move
             this.e.applyVectors();
             
-            if (keysDown.Contains(Keys.Left)) {
+            // When the Left arrow is pressed
+            if (client.isKeyPressed(Keys.Left)) {
                 Vector v = new Vector(Axis.X);
                 v.setForce(2);
                 v.setSpeed(Direction.LEFT, 5);
                 this.e.getLocation().setVectorX(v);
             }
 
-            if (keysDown.Contains(Keys.Right)) {
+            // When the right arrow is pressed
+            if (client.isKeyPressed(Keys.Right)) {
                 Vector v = new Vector(Axis.X);
                 v.setForce(2);
                 v.setSpeed(Direction.RIGHT, 5);
                 this.e.getLocation().setVectorX(v);
             }
 
-            if (keysDown.Contains(Keys.Up) && this.e.isOnGround()) {
+            // When the up arrow is pressed
+            if (client.isKeyPressed(Keys.Up) && this.e.isOnGround()) {
                 Vector v = new Vector(Axis.Y);
                 v.setForce(9);
                 v.setSpeed(4);
@@ -59,15 +81,16 @@ namespace ComputerScienceUtilities
             t.Start();
         }
 
+        // When the user presses down on a key on the keyboard
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if (keysDown.Contains(e.KeyCode)) return;
-            keysDown.Add(e.KeyCode);
+            client.pressKey(e.KeyCode);
         }
 
+        // When the user releases a key on the keyboard
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-                keysDown.Remove(e.KeyCode);
+            client.releaseKey(e.KeyCode);
         }
     }
 }
