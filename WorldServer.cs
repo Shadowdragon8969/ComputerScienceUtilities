99259c4142577cd04e11911f215ca8e15c10eeb0
Shadowdragon8969 <shadowdragon8969@gmail.com>
2017-11-06 13:43:11 -0600

    Update WorldServer.cs

--- WorldServer.cs
+++ WorldServer.cs
@@ -12,6 +12,9 @@ namespace ComputerScienceUtilities
         private static Timer t = new Timer();
         private static List<World> worlds = new List<World>();
         public static String testString = "hhh";
+        public static int tickSpeed = 20;
+        private static Client cl;
+        private static EntityPlayer ep;
 
         public static void registerWorld(World w) {
             worlds.Add(w);
@@ -21,8 +24,51 @@ namespace ComputerScienceUtilities
             return worlds;
         }
 
-        public void initWorldServer()
+        public static void initWorldServer(Client c, EntityPlayer enp)
         {
+            cl = c;
+            ep = enp;
+            t.Tick += new EventHandler(serverTickElapsed);
+            t.Interval = tickSpeed;
+            t.Enabled = true;
+            t.Start();
+        }
+
+        private static void serverTickElapsed(object sender, EventArgs e)
+        {
+            t.Stop();
+            foreach (Entity ent in Entity.getRegisteredEntities())
+            {
+                ent.applyVectors();
+                if (ent is EntityLiving && !(ent is EntityPlayer))
+                {
+                    EntityLiving el = (EntityLiving)ent;
+                    el.aiMovement();
+                }
+                if (ent is EntityPlayer)
+                {
+                    EntityPlayer ep = (EntityPlayer)ent;
+                    ep.aiMovement();
+                }
+            }
+            if (cl.getKeysDown().Contains(cl.left.getKey())) {
+                Vector v = new Vector(Axis.X);
+                v.setForce(4);
+                v.setSpeed(Direction.LEFT, ep.getMovementSpeed());
+                ep.getLocation().setVectorX(v);
+            }
+            if (cl.getKeysDown().Contains(cl.right.getKey()))
+            {
+                Vector v = new Vector(Axis.X);
+                v.setForce(4);
+                v.setSpeed(Direction.RIGHT, ep.getMovementSpeed());
+                ep.getLocation().setVectorX(v);
+            }
+            if (cl.getKeysDown().Contains(cl.up.getKey()))
+            {
+                ep.jump();
+            }
+            t.Start();
         }
 
     }
