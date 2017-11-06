723595c821cb41b3283e6dc06e50fc166e21b30c
Shadowdragon8969 <shadowdragon8969@gmail.com>
2017-11-06 13:40:43 -0600

    Create EntityPlayer.cs

---
+++ EntityPlayer.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerScienceUtilities
+{
+    public class EntityPlayer : EntityLiving
+    {
+
+        public EntityPlayer(PictureBox box, int health, int maxhealth, string name, Label label) : base(box, health, maxhealth, name, label)
+        {
+            setHostile(false);
+        }
+
+        public void jump() {
+            if (!isOnGround()) return;
+            Vector v = new Vector(Axis.Y);
+            v.setForce(10);
+            v.setSpeed(getJumpHeight());
+            getLocation().setVectorY(v);
+        }
+
+    }
+}
